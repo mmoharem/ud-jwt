b1cb7e78a663b39780663d5a987cb57a4b87c2bf
mmoharem <cloud@bnfstore.com>
2018-10-08 00:10:04 +0200

    after canactivate

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,5 +13,6 @@ Route::group([
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
+    Route::post('sendPassResetLink', 'ResetPassController@sendEmail');
 
 });
